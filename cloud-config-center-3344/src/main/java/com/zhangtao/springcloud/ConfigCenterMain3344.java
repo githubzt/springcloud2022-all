ffb5562d724813606a7c159df05b8e072bbee259
zhangtao1 <zhangtao1@95303.com>
2022-11-24 17:51:38 +0800

    提交springcloud工程

--- cloud-config-center-3344/src/main/java/com/zhangtao/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/zhangtao/springcloud/ConfigCenterMain3344.java
@@ -2,6 +2,7 @@ package com.zhangtao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ConfigCenterMain3344 {
 
     public static void main(String[] args) {
